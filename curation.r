0977755cdaf403d92cc641dbd44a1934b1ac8050
samdoc829 <sam_aanjos@usp.br>
2024-07-15 09:33:05 -0300

    Finalazing script and adding the final families csv

--- curation.r
+++ curation.r
@@ -1,5 +1,5 @@
 # Carregando os dados
-raw_data <- read.csv("C:/Users/sam_a/OneDrive/Documentos/Mestrado/Projeto/curadoria/Original.csv")
+raw_data <- read.csv("G:/Outros computadores/Meu computador/Mestrado/Projeto/Curation/Original.csv")
 
 # Invertendo a ordem das colunas 4 e 5
 raw_data <- raw_data[, c(1,2, 5, 4, 6, 3)]
@@ -28,3 +28,33 @@ final_data <- subset(ordered_data, region != "Southern America" & region != "Cen
 
 # Salvar os dados ordenados e limpos em um novo arquivo (substitua 'novo_arquivo.csv' pelo nome desejado)
 write.csv(final_data, file = "cleaned_data.csv", row.names = FALSE)
+
+# Carregando os data_pre_selection (substitua 'seu_arquivo.csv' pelo nome do seu arquivo CSV)
+data_pre_selection <- read.csv("cleaned_data.csv")
+
+# Calculando o nÃºmero de linhas Ãºnicas por famÃ­lia na coluna de espÃ©cies
+sp_per_family <- tapply(data_pre_selection$species, data_pre_selection$family, function(x) length(unique(x)))
+
+# Identificando as famÃ­lias com menos de 9 linhas Ãºnicas
+families_to_keep <- names(sp_per_family[sp_per_family >= 9])
+
+# Criando uma nova tabela apenas com as famÃ­lias que possuem 9 ou mais linhas Ãºnicas
+selected_data <- data_pre_selection[data_pre_selection$family %in% families_to_keep, ]
+
+# Escrevendo a nova tabela em um novo arquivo CSV (substitua 'novo_arquivo.csv' pelo nome desejado)
+write.csv(selected_data, file = "selected_families_data.csv", row.names = FALSE)
+
+# Carregando os dados (substitua 'seu_arquivo.csv' pelo nome do seu arquivo CSV)
+selected_families_data <- read.csv("selected_families_data.csv")
+
+# Dividindo o dataframe com base na coluna de famílias e ordenando por região
+frames_list <- split(selected_families_data, selected_families_data$family)
+
+# Ordenando cada subconjunto por região
+frames_list <- lapply(frames_list, function(subset) subset[order(subset$region), ])
+
+# Salvando cada subconjunto de dados em arquivos separados
+for (i in names(frames_list)) {
+  nome_arquivo <- paste(i, ".csv", sep = "")
+  write.csv(frames_list[[i]], file = nome_arquivo, row.names = FALSE)
+}
